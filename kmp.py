dc96de90b27a30728cbe68a4ca277a29b2f2a6f9
Doug Tomm <dtomm@radiumone.com>
2018-01-06 06:59:43 -0800

    implemented KMP matching algorithm

--- kmp.py
+++ kmp.py
@@ -30,3 +30,43 @@ def overlap(pattern):
                 k += 1
 
     return f
+
+
+def match(pattern, text):
+    """
+
+    :param pattern:
+    :param text:
+    :return:  list of indices in text where pattern matches; may be empty.
+    """
+    overlaps = overlap(pattern)
+    result = []
+
+    len_pattern = len(pattern)
+    len_text = len(text)
+    k_bound = len_text - len_pattern + 1
+
+    k = 0
+    while k < k_bound:
+        x = 0
+        for p in pattern:
+            if p != text[k + x]:
+                break
+            x += 1
+
+        if x == len_pattern:
+            # found a match
+            result.append(k)
+            k += x
+            continue
+
+        if x == 0:
+            k += 1
+            continue
+
+        bump = x - overlaps[x - 1]
+        if bump < 1:
+            raise Exception("bump = %s, k = %s, x = %s" % (bump, k, s))
+        k += bump
+
+    return result
--- test_kmp.py
+++ test_kmp.py
@@ -20,3 +20,47 @@ class TestKMP(unittest.TestCase):
         f = kmp.overlap(pattern)
         self.assertTrue(len(pattern) == len(f))
         self.assertEqual(0, f[0])
+
+    def test_match(self):
+        pattern = 'abcdabd'
+        text = 'abc abcdab abcdabcdabde'
+        result = kmp.match(pattern, text)
+        self.assertEqual([15], result)
+
+    def test_pattern_is_text(self):
+        pattern = 'abcdabd'
+        result = kmp.match(pattern, pattern)
+        self.assertEqual([0], result)
+
+    def test_no_match(self):
+        pattern = 'zzyzyvq'
+        text = 'abc abcdab abcdabcdabde'
+        result = kmp.match(pattern, text)
+        self.assertEqual([], result)
+
+    def test_gattaca(self):
+        sequence = (
+            'GACTTTTTTTTTTTTTCCTTTGGGAAAGGTAGGGAGGTGTTCGTACGGGAGCAGCCTCGG'
+            'GGACCCCTGCACTGGGTCAGGGCTTATGAAGCTAGAAGCGTCCCTCTGTTCCCTTTGTGA'
+            'GTTGGTGGGTTGTTGGTACATTTGGTTGGAAGCTGTGTTGCTGGTTAGGGAGACTCGGTT'
+            'TTGCTCCTTGGGTTCGAGGAAAGCTGGAGAATAGAAGCCATTGTTTGCCGTCTGTCGGCT'
+            'TTGTCGACCACGCTCACCCCCTCCTGTTCGTACTTTTTAAAGCAGTGAGGCGAGGTAGAC'
+            'AGGGTGTGTCACAGTACAGTTAAAGGGGTGAAGATCTAAACGCCAAAAGAGAAGTTAATC'
+            'ACAATAAGTGAGGTTTGGGATAAAAAGTTGGGCTTGCCCCTTTCAAAGTCCCAGAAAGCT'
+            'GGGAGGTAGATGGAGAGGGGGCCATTGGGAAGTTTTTTTGGTGTAGGGAGAGGAGTAGAA'
+            'GATAAAGGGTAAGCAGAGTGTTGGGTTCTGGGGGTCTTGTGAAGTTCCTTAAGGAAGGAG'
+            'GGAGTGTGGCCCTGCAGCCCTCCCAAACTGCTCCAGCCTATGCTCTCCGGCACCAGGAAG'
+            'TTCCAAGGTTCCCTTCCCCTGGTCTCCAAACTTCAGGTATTCCTCTCCCCTCACACCCCT'
+            'TCAACCTCAGCTCTTGGCCTCTACTCCTTACTCCACTGTTCCTCCTGTTTCCCCCTTCCC'
+            'CTTTTCCTGGTTCTTTATATTTTTGCAAAGTGGGATCCGAACTTGCTAGATTTTCCAATT'
+            'CTCCCAAGCCAGACCAGAGCAGCCTCTTTTAAAGGATGGAGACTTCTGTGGCAGATGCCG'
+            'CTGAAAATGTGGGTGTAATGCTGGGACTTAGAGTTTGATGACAGTTTGACTGAGCCCTAG'
+            'ATGCATGTGTTTTTCCTGAGAGTGAGGCTCAGAGAGCCCATGGACGTATGCTGTTGAACC'
+            'ACAGCTTGATATACCTTTTTCTCCTTCTGTTTTGTCTTAGGGGGAAGACTTTAACTAGGG'
+            'GCGCGCAGATGTGTGAGGCCTTTTATTGTGAGAGTGGACAGACATCCGAGATTTCAGGCA'
+            'AGTTCTGTGGTGGCTGCTTTGGGCT'
+        )
+
+        print len(sequence)
+        print kmp.match('CAT', sequence)
+        print kmp.match('GGCAA', sequence)
